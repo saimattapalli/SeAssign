721eb1141f5462b590e057d75cfd496de9af5b3f
saimattapalli <111996167+saimattapalli@users.noreply.github.com>
2022-10-09 21:21:52 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from './Components/NavbarComp';
+import NewsList from './Components/NewsList';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <NavbarComp />
+      
+
+    </div>
+  );
+}
+
+export default App;
